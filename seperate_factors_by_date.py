dcab68b340cde949695e56b6b81be556a9025c6e
pratikjoy7 <pratikjoy7@gmail.com>
2017-04-23 01:02:08 +0600

    Refactor the weather data parsing script to separate weather data parsing by month

--- seperate_factors_by_date.py
+++ seperate_factors_by_date.py
@@ -3,6 +3,7 @@ import csv
 import os
 
 def main(file):
+	file_ext = file.split('/')[0]
 	with open(file, 'r+') as raw_file:
 		reader = csv.reader(raw_file)
 		for row in reader:
@@ -19,18 +20,18 @@ def main(file):
 
 			if not os.path.exists('../parsed'):
 				os.makedirs('../parsed')
-				open('../parsed/date_time_vs_humidity.txt', 'a').close()
-				open('../parsed/date_time_vs_visibility.txt', 'a').close()
-				open('../parsed/date_time_vs_wind_speed.txt', 'a').close()
-				open('../parsed/date_time_vs_condition.txt', 'a').close()
+			open('parsed/date_time_vs_humidity_' + file_ext + '.txt', 'a').close()
+			open('parsed/date_time_vs_visibility_' + file_ext + '.txt', 'a').close()
+			open('parsed/date_time_vs_wind_speed_' + file_ext + '.txt', 'a').close()
+			open('parsed/date_time_vs_condition_' + file_ext + '.txt', 'a').close()
 
 			if row[13] == 'DateUTC':
 				continue
 			else:
-				write_rows('../parsed/date_time_vs_humidity.txt', date_time_vs_humidity)
-				write_rows('../parsed/date_time_vs_visibility.txt', date_time_vs_visibility)
-				write_rows('../parsed/date_time_vs_wind_speed.txt', date_time_vs_condition)
-				write_rows('../parsed/date_time_vs_condition.txt', date_time_vs_condition)
+				write_rows('parsed/date_time_vs_humidity_' + file_ext + '.txt', date_time_vs_humidity)
+				write_rows('parsed/date_time_vs_visibility_' + file_ext + '.txt', date_time_vs_visibility)
+				write_rows('parsed/date_time_vs_wind_speed_' + file_ext + '.txt', date_time_vs_condition)
+				write_rows('parsed/date_time_vs_condition_' + file_ext + '.txt', date_time_vs_condition)
 
 def write_rows(filename, data):
 	with open(filename, 'a+') as parsed_file:
