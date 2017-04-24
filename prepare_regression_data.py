4ffa520f720c72ad6064e4c72b3685ae78a52bb6
pratikjoy7 <pratikjoy7@gmail.com>
2017-04-24 14:57:16 +0600

    Finalize parser to generate dataset for regression analysis

--- prepare_regression_data.py
+++ prepare_regression_data.py
@@ -1,18 +1,18 @@
 import argparse
 import csv
 import os
-from datetime import datetime
+import datetime
 import re
 
-def main(file):
-	with open('parsed/date_time_vs_humidity_january-2011.csv', 'r+') as file:
+def main(weather_file, retail_file):
+	with open(weather_file, 'r+') as file:
 		read_file = csv.reader(file)
 		for line in read_file:
 			weather_date = line[0]
-			weather_time = line[1]
+			time_range_start = datetime.datetime.strptime(line[1], '%H:%M:%S')
 			factor = line[2]
-			weather_time_last_range = '%s%02d:00' % (weather_time.split(':')[0], int(weather_time.split(':')[1]) + 30)
-			with open('retail-data-uk-demo.csv', 'r+') as raw_file:
+			time_range_end = time_range_start + datetime.timedelta(minutes = 30)
+			with open(retail_file, 'r+') as raw_file:
 				reader = csv.reader(raw_file)
 				for row in reader:
 					description = re.sub(' +',' ', re.sub(r"[^\w\s]", '', row[2]))
@@ -28,18 +28,18 @@ def main(file):
 						month = '20%s-%s' % (date.split('/')[2], date.split('/')[0])
 						date = '20%02d-%02d-%02d' % (int(date.split('/')[2]), int(date.split('/')[0]), int(date.split('/')[1]))
 						time = '%02d:%02d:00' % (int(time.split(':')[0]), int(time.split(':')[1]))
+						time_obj = datetime.datetime.strptime(time, '%H:%M:%S')
 						invoice_date = '%s %s' % (date, time)
 
 						if date == weather_date:
-							if weather_time <= time < weather_time_last_range:
-								print weather_time_last_range + ' - ' + weather_time
+							if time_range_start <= time_obj < time_range_end:
 								parsed_retail_data = [unique_id, invoice_date, description, quantity, unit_price, date, time, factor]
 
-								if not os.path.exists('demo'):
-									os.makedirs('demo')
-								open('demo/parsed_retail_data_' + month + '.txt', 'a').close()
+								if not os.path.exists('parsed'):
+									os.makedirs('parsed')
+								open('parsed/parsed_retail_data_' + month + '.txt', 'a').close()
 
-								write_rows('demo/parsed_retail_data_' + month + '.txt', parsed_retail_data)
+								write_rows('parsed/parsed_retail_data_' + month + '.txt', parsed_retail_data)
 							else:
 								continue
 						else:
@@ -54,6 +54,7 @@ def write_rows(filename, data):
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='This script is intended to parse a csv file of weather data and extract useful information')
-	parser.add_argument('--file', help='Specify the filename that needs to be parsed')
+	parser.add_argument('--weather', help='Specify the weather filename that needs to be parsed')
+	parser.add_argument('--retail', help='Specify the retail data filename that needs to be parsed')
 	args = parser.parse_args()
-	main(args.file)
+	main(args.weather, args.retail)
