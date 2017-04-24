cb9334042a784305e53ab4988c998ba193ac7ae7
pratikjoy7 <pratikjoy7@gmail.com>
2017-04-24 16:50:37 +0600

    Update script that generates dataset for regression analysis

--- prepare_regression_data.py
+++ prepare_regression_data.py
@@ -5,41 +5,40 @@ import datetime
 import re
 
 def main(weather_file, retail_file):
-	with open(weather_file, 'r+') as file:
-		read_file = csv.reader(file)
-		for line in read_file:
-			weather_date = line[0]
-			time_range_start = datetime.datetime.strptime(line[1], '%H:%M:%S')
-			factor = line[2]
+	with open(weather_file, 'r+') as weather_file:
+		weather_file_reader = csv.reader(weather_file)
+		for weather_row in weather_file_reader:
+			weather_date = weather_row[0]
+			time_range_start = datetime.datetime.strptime(weather_row[1], '%H:%M:%S')
+			factor = weather_row[2]
 			time_range_end = time_range_start + datetime.timedelta(minutes = 30)
-			with open(retail_file, 'r+') as raw_file:
-				reader = csv.reader(raw_file)
-				for row in reader:
-					description = re.sub(' +',' ', re.sub(r"[^\w\s]", '', row[2]))
-					quantity = row[3]
-					invoice_date = row[4]
-					unit_price = row[5]
-					unique_id = row[8]
+			
+			with open(retail_file, 'r+') as retail_file:
+				retail_file_reader = csv.reader(retail_file)
+				for retail_row in retail_file_reader:
+					unique_id = retail_row[0]
+					invoice_date = retail_row[1]
+					description = retail_row[2]
+					quantity = retail_row[3]
+					unit_price = retail_row[4]
+					date = retail_row[5]
+					time = retail_row[6]
+					month = '20%s-%s' % (date.split('-')[0], date.split('-')[1])
 
-					if row[2] == 'Description':
+					if retail_row[2] == 'Description':
 						continue
 					try:
-						date,time = invoice_date.split()
-						month = '20%s-%s' % (date.split('/')[2], date.split('/')[0])
-						date = '20%02d-%02d-%02d' % (int(date.split('/')[2]), int(date.split('/')[0]), int(date.split('/')[1]))
-						time = '%02d:%02d:00' % (int(time.split(':')[0]), int(time.split(':')[1]))
 						time_obj = datetime.datetime.strptime(time, '%H:%M:%S')
-						invoice_date = '%s %s' % (date, time)
 
 						if date == weather_date:
 							if time_range_start <= time_obj < time_range_end:
-								parsed_retail_data = [unique_id, invoice_date, description, quantity, unit_price, date, time, factor]
+								regression_retail_data = [unique_id, invoice_date, description, quantity, unit_price, factor]
 
-								if not os.path.exists('parsed'):
-									os.makedirs('parsed')
-								open('parsed/parsed_retail_data_' + month + '.txt', 'a').close()
+								if not os.path.exists('regression_dataset'):
+									os.makedirs('regression_dataset')
+								open('regression_dataset/regression_retail_data_' + month + '.txt', 'a').close()
 
-								write_rows('parsed/parsed_retail_data_' + month + '.txt', parsed_retail_data)
+								write_rows('regression_dataset/regression_retail_data_' + month + '.txt', regression_retail_data)
 							else:
 								continue
 						else:
@@ -48,8 +47,8 @@ def main(weather_file, retail_file):
 						continue
 
 def write_rows(filename, data):
-	with open(filename, 'a+') as parsed_file:
-		writer = csv.writer(parsed_file, dialect='excel')
+	with open(filename, 'a+') as regression_file:
+		writer = csv.writer(regression_file, dialect='excel')
 		writer.writerow(data)
 
 if __name__ == '__main__':
